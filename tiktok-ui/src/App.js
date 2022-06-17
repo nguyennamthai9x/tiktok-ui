f615ba11e43049388ab43928afab9468ee8c8b82
unknown <nguyennamthai9x@gmail.com>
2022-06-17 17:04:07 +0700

    setup Router/Layout for project

--- tiktok-ui/src/App.js
+++ tiktok-ui/src/App.js
@@ -1,10 +1,35 @@
-
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {publicRoutes} from '~/routes';
+import {DefaultLayout} from '~/components/Layout'
 function App() {
-  return (
-    <div className="App">
-        <h1>React App</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        let Layout = DefaultLayout
+
+                        if(route.layout) {
+                            Layout = route.layout
+                        }else if(route.layout === null){
+                            Layout = DefaultLayout
+                        }
+                        const Page = route.component
+                        return <Route 
+                            key={index} 
+                            path={route.path} 
+                            element={
+                                <Layout>
+                                    <Page />
+                                </Layout>
+                            }
+                            />
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
