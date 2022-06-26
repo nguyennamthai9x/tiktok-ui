af5939396fa1be5d5f522d8f96f0e5c7908b3eda
unknown <nguyennamthai9x@gmail.com>
2022-06-26 22:11:05 +0700

    UI header-6

--- tiktok-ui/src/components/Layout/components/Header/index.js
+++ tiktok-ui/src/components/Layout/components/Header/index.js
@@ -1,6 +1,7 @@
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Tippy from '@tippyjs/react/headless';
+import Tippy from '@tippyjs/react';
+import HeadlessTippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css';
 
 import styles from './Header.module.scss';
@@ -14,9 +15,15 @@ import {
     faEllipsisVertical,
     faEarthAsia,
     faCircleQuestion,
-    faKeyboard 
+    faKeyboard, 
+    faCloudUpload,
+    faGear,
+    faCoins,
+    faUser,
+    faSignOut,
+    // faMessage
 } from '@fortawesome/free-solid-svg-icons';
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import {Wrapper as PopperWrapper} from '~/components/Popper'; 
 import AccountItem from '~/components/AccountItem';
 import Menu from '~/components/Popper/Menu';
@@ -55,6 +62,8 @@ const MENU_ITEMS = [
 ]
 
 function Header() {
+
+    const currentUser = true;
     const [searchResults, setSearchResults] = useState([]);
 
     useEffect(() =>{
@@ -72,12 +81,37 @@ function Header() {
         }
     }
 
+    const userMenu = [
+        {
+            icon: <FontAwesomeIcon icon={faUser} />,
+            title: 'View profile',
+            to: '/@Midu',
+        },
+        {
+            icon: <FontAwesomeIcon icon={faCoins} />,
+            title: 'Get coins',
+            to: '/coin',
+        },
+        {
+            icon: <FontAwesomeIcon icon={faGear} />,
+            title: 'Settings',
+            to: '/settings',
+        },
+        ...MENU_ITEMS,
+        {
+            icon: <FontAwesomeIcon icon={faSignOut} />,
+            title: 'Log out',
+            to: '/logout',
+            separate: true,
+        },
+    ]
+
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <img src={images.logo} alt="TikTok"/>
 
-                <Tippy
+                <HeadlessTippy
                     interactive
                     visible={searchResults.length > 0}
                     render={attrs => (
@@ -105,21 +139,47 @@ function Header() {
                                 <FontAwesomeIcon icon={faMagnifyingGlass} />
                             </button>
                     </div>
-                </Tippy>
+                </HeadlessTippy>
 
                 <div className={cx('actions')}>
-                    <Button text>Upload</Button>
-                    <Button primary >Log in</Button>
+                    {currentUser ? (
+                        <>
+                            <Tippy
+                                delay={[0, 200]}
+                                content="Upload video"
+                                placement='bottom'
+                            >
+                                <button className={cx('action-btn')}>
+                                    <FontAwesomeIcon icon={faCloudUpload} />
+                                </button>
+                            </Tippy>
+                        </>
+                    
+                    ):(
+                        <>
+                            <Button text>Upload</Button>
+                            <Button primary >Log in</Button>
+                        </>
+                    )}
 
                     <Menu
-                        items={MENU_ITEMS}
+                        items={userMenu}
                         onChange={handleMenuChange}
                     >
-                        <button className={cx('more-btn')}>
-                            <FontAwesomeIcon icon={faEllipsisVertical} />
-                        </button>
+                        {currentUser ? (
+                            <img 
+                                className={cx('user-avatar')} 
+                                src="https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2022/2/20/1016092/Midu-04.jpeg"
+                                alt="Midu"
+                            />
+                        ):(
+                            <button className={cx('more-btn')}>
+                                <FontAwesomeIcon icon={faEllipsisVertical} />
+                            </button>
+                        )}
                     </Menu>
                 </div>
+
             </div>
         </header>
     );
--- tiktok-ui/src/components/Popper/Menu/index.js
+++ tiktok-ui/src/components/Popper/Menu/index.js
@@ -18,6 +18,7 @@ function Menu( { children, items= [], onChange = defaultFn }) {
     const [history, setHistory] = useState([{data:items}])
     const current = history[history.length - 1] 
 
+    // console.log(items);
     const renderItems = () => {
         return current.data.map((item, index) => {
             const isParrent = !!item.children
@@ -43,6 +44,7 @@ function Menu( { children, items= [], onChange = defaultFn }) {
         <Tippy
             interactive
             delay={[0, 700]}
+            offset={[12, 8]}
             placement="bottom-end"
             render={(attrs) => (
                 <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
@@ -54,6 +56,7 @@ function Menu( { children, items= [], onChange = defaultFn }) {
                     </PopperWrapper>
                 </div>
             )}
+            onHide={() => setHistory(prev => prev.slice(0,1))}
         >
             {children}
         </Tippy>
