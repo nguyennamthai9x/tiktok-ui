926d4b2915e131e73e534c03150d6ae31cb51f64
unknown <nguyennamthai9x@gmail.com>
2022-07-01 10:07:17 +0700

    UI Sidebar-1

---
+++ tiktok-ui/src/layouts/components/Sidebar/index.js
@@ -0,0 +1,45 @@
+import classNames from 'classnames/bind';
+import config from '~/config';
+import styles from './Sidebar.module.scss';
+
+import Menu from './Menu/';
+import MenuItem from './Menu/MenuItem';
+import {
+    HomeIcon,
+    UserGroupIcon,
+    LiveIcon,
+    HomeActiveIcon,
+    UserGroupActiveIcon,
+    LiveActiveIcon,
+} from '~/components/Icons';
+
+const cx = classNames.bind(styles);
+
+function Sidebar() {
+    return (
+        <aside className={cx('wrapper')}>
+            <Menu>
+                <MenuItem
+                    title="For You"
+                    to={config.routes.home}
+                    icon={<HomeIcon />}
+                    activeIcon={<HomeActiveIcon />}
+                />
+                <MenuItem
+                    title="Following"
+                    to={config.routes.following}
+                    icon={<UserGroupIcon />}
+                    activeIcon={<UserGroupActiveIcon />}
+                />
+                <MenuItem
+                    title="Live"
+                    to={config.routes.live}
+                    icon={<LiveIcon />}
+                    activeIcon={<LiveActiveIcon />}
+                />
+            </Menu>
+        </aside>
+    );
+}
+
+export default Sidebar;
